4dbb5ee88dbd7eedcc77cfb7977f7d03e978cf5f
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-03-29 19:36:37 -0600

    Add password comparer

---
+++ script.js
@@ -0,0 +1,26 @@
+let pass = document.getElementById("password");
+let veripass = document.getElementById("veripass");
+let message = document.getElementById("message");
+
+// document.getElementById("fname").onchange = function() {myFunction()};
+
+// function myFunction() {
+//   var x = document.getElementById("fname");
+//   x.value = x.value.toUpperCase();
+// }
+
+
+veripass.onchange = function () {checker()};
+
+function checker() {
+    if(veripass.value != pass.value){
+        veripass.classList.add("error");
+        message.innerHTML = "The passwords don't match!";
+        message.classList.add("errortext");
+    }
+    else{
+        veripass.classList.remove("error");
+        message.classList.remove("errortext");
+        message.innerHTML = " ";
+    }
+}
