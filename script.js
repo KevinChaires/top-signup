3329cddf1bc73c44cb0b505098050ecdf3d95aa8
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-03-29 19:40:50 -0600

    Edit script.js comments

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ let message = document.getElementById("message");
 veripass.onchange = function () {checker()};
 
 function checker() {
-    if(veripass.value != pass.value){
-        veripass.classList.add("error");
-        message.innerHTML = "The passwords don't match!";
-        message.classList.add("errortext");
+    if(veripass.value != pass.value){ //Si no son iguales
+        veripass.classList.add("error"); //Rojo
+        pass.classList.add("error"); //Rojo
+        message.innerHTML = "The passwords don't match!"; //Mensaje de error
+        message.classList.add("errortext"); //Rojo
     }
     else{
-        veripass.classList.remove("error");
+        veripass.classList.remove("error"); //Si si son iguales
+        pass.classList.remove("error"); 
         message.classList.remove("errortext");
         message.innerHTML = " ";
     }
